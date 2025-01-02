965acebdfcd7461cc78d5a243c0d61256f1e7f26
hani <haniknight2003@gmail.com>
2025-01-02 21:37:54 +0200

    First Submission

---
+++ cat-portfolio/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import './components/navbar'
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ cat-portfolio/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <nav className={`fixed w-full bg-black font-Bahn`}>
+      
+    </nav>
+  )
+}
+export default Navbar;
